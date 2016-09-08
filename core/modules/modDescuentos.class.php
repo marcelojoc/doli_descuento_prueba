e44cbd4290dd866174b021de0d707d8d735a5870
Marcelo Contreras <marcelojoc@hotmail.com>
2016-09-08 02:50:12 -0300

    pantalla limpia

--- core/modules/modDescuentos.class.php
+++ core/modules/modDescuentos.class.php
@@ -91,7 +91,7 @@ class modDescuentos extends DolibarrModules
 		//							'dir' => array('output' => 'othermodulename'),      // To force the default directories names
 		//							'workflow' => array('WORKFLOW_MODULE1_YOURACTIONTYPE_MODULE2'=>array('enabled'=>'! empty($conf->module1->enabled) && ! empty($conf->module2->enabled)', 'picto'=>'yourpicto@descuentos')) // Set here all workflow context managed by module
 		//                        );
-		$this->module_parts = array();
+		$this->module_parts = array('css' => array('/descuentos/css/descuentos.css.php'));
 
 		// Data directories to create when module is enabled.
 		// Example: this->dirs = array("/descuentos/temp");
